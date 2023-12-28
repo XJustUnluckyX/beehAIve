3212a7acbb7f820ac608fc2050a9acdca9e3f01e
MonTheDog <cate.party@gmail.com>
2023-12-28 19:05:22 +0100

    Fixed Database Connection

--- src/main/java/it/unisa/c10/beehAIve/BeehAIveApplication.java
+++ src/main/java/it/unisa/c10/beehAIve/BeehAIveApplication.java
@@ -2,9 +2,8 @@ package it.unisa.c10.beehAIve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class BeehAIveApplication {
 
 	public static void main(String[] args) {
