bee5d5d12c12b99f568d5ef414962fdf6c0b1340
MonTheDog <cate.party@gmail.com>
2024-01-03 16:10:30 +0100

    Pipeline di preparazione file audio e data cleaning iniziati

---
+++ src/ai/audio_files.py
@@ -0,0 +1,115 @@
+import gc
+import os
+from multiprocessing import Process
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+from pydub import AudioSegment
+
+# Directory con le clip audio
+dir = "D:\Desktop\\fafo\sound_files"
+dir2 = "D:\Desktop\\fafo\merge"
+dir3 = "D:\Desktop\\fafo\spectrograms"
+#dir = "D:\Desktop\dataset\sound_files\sound_files"
+
+audio_list = os.listdir(dir)
+
+# Definiamo dei set per trovare i file con meno di tre segmenti
+# segment0_set = set()
+# segment1_set = set()
+# segment2_set = set()
+
+# for file in audio_list:
+#     if (file.endswith("segment0.wav")):
+#         file = file[:-14]
+#         segment0_set.add(file)
+#     if (file.endswith("segment1.wav")):
+#         file = file[:-14]
+#         segment1_set.add(file)
+#     if (file.endswith("segment2.wav")):
+#         file = file[:-14]
+#         segment2_set.add(file)
+
+# Set di file da cancellare
+# set_to_remove1 = segment0_set.difference(segment1_set)
+# set_to_remove2 = segment0_set.difference(segment2_set)
+
+# Loop per cancellare i segmenti 0 e 1 dove necessario
+# for file in audio_list:
+#     file = file[:-14]
+#     if file in set_to_remove1:
+#         if os.path.exists(dir + "\\" + file + "__segment0.wav"):
+#             file = file + "__segment0.wav"
+#             os.remove(dir + "\\" + file)
+#     if file in set_to_remove2:
+#         if os.path.exists(dir + "\\" + file + "__segment0.wav"):
+#             file = file + "__segment0.wav"
+#             os.remove(dir + "\\" + file)
+#         if os.path.exists(dir + "\\" + file + "__segment1.wav"):
+#             file = file + "__segment1.wav"
+#             os.remove(dir + "\\" + file)
+
+# Loop per cancellare tutti i segmenti 3, 4 e 5
+# for file in audio_list:
+#     file = file[:-14]
+#     if os.path.exists(dir + "\\" + file + "__segment3.wav"):
+#         file = file + "__segment3.wav"
+#         os.remove(dir + "\\" + file)
+#     if os.path.exists(dir + "\\" + file + "__segment4.wav"):
+#         file = file + "__segment4.wav"
+#         os.remove(dir + "\\" + file)
+#     if os.path.exists(dir + "\\" + file + "__segment5.wav"):
+#         file = file + "__segment5.wav"
+#         os.remove(dir + "\\" + file)
+
+# Unisce i vari segmenti in un unico file
+# for i in (range(int(len(audio_list)/3))):
+#     segment0 = AudioSegment.from_wav(dir + "\\" + audio_list[i * 3])
+#     segment1 = AudioSegment.from_wav(dir + "\\" + audio_list[i * 3 + 1])
+#     segment2 = AudioSegment.from_wav(dir + "\\" + audio_list[i * 3 + 2])
+#     merged_file_name = audio_list[i * 3][:-14] + ".wav"
+#     merged = segment0 + segment1 + segment2
+#     merged.export(dir2 + "\\" + merged_file_name, format="wav")
+
+
+def spectrograms(i):
+    file_list = os.listdir(dir2)
+    file_list2 = os.listdir(dir3)
+    for item in range(i * 239, (i+1) * 239 -1):
+        file = file_list[item]
+        if (file[:-4] + "_spect.png") in file_list2:
+            continue
+        else:
+            print(file + " iniziato")
+            y, sr = librosa.load(dir2 + "\\" + file, sr=None) # Caricamento del file audio
+            spect = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max) # Realizzazione dello spettrogramma
+            librosa.display.specshow(spect, sr=sr, x_axis='time', y_axis='log') # Visualizzazione dello spettrogramma
+            plt.axis("off")  # Eliminazione degli assi dall'immagine prodotta
+            plt.subplots_adjust(left=0, right=1, top=1, bottom=0)  # Eliminazione del padding bianco attorno lo spettrogramma
+            plt.savefig(dir3 + "\\" + file[:-4] + "_spect.png") # Salvataggio dello spettrogramma nella cartella
+            del y, sr, spect
+            gc.collect()
+            print(file + " salvato")
+
+
+if __name__ == '__main__': # Si assicura l'esecuzione del seguente blocco solo dal processo principale, così
+    # da non essere eseguito da alcun processo figlio
+    p1 = Process(target=spectrograms, args=[0])
+    p2 = Process(target=spectrograms, args=[1])
+    p3 = Process(target=spectrograms, args=[2])
+    p4 = Process(target=spectrograms, args=[3])
+    p5 = Process(target=spectrograms, args=[4])
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    p5.start()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+
+# if __name__ == '__main__':
+#     with multiprocessing.Pool(5) as p:
+#         p.map(spectrograms, [0,1,2,3,4])
---
+++ src/ai/data_cleaning.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import os
+
+#TODO rendere il codice leggibile
+
+# Path del CSV del dataset
+dataset_path = os.path.dirname(__file__) + "\dataset\\HiveDataset.csv"
+merged_audio_dir = "D:\Desktop\\fafo\merge"
+
+
+# Lettura CSV
+ds = pd.read_csv(dataset_path)
+
+ds["temp diff"] = ds["hive temp"] - ds["weather temp"]
+ds["humidity diff"] = ds["hive humidity"] - ds["weather humidity"]
+
+# Eliminiamo le colonne non necessarie al nostro problema
+ds = ds.drop(["device", "hive number", "hive pressure", "weather pressure", "wind speed", "gust speed", "weatherID",
+             "cloud coverage", "rain", "lat", "long", "queen acceptance", "frames", "target", "queen status",
+              "date", "time", "hive temp", "weather temp", "hive humidity", "weather humidity"], axis=1)
+
+# Pulitura nome file
+ds["file name"] = ds["file name"].str[:-4] + ".wav"
+
+# Rimozione righe del dataset di cui non possediamo il file audio, anche a conseguenza del merge dei file audio
+merged_audio_names = os.listdir(merged_audio_dir)
+ds_filename_list = ds['file name'].tolist()
+
+filename_to_remove = set(ds_filename_list) - set(merged_audio_names)
+
+# Produciamo un nuovo dataframe rimuovendo le righe di cui non abbiamo il file
+ds2 = ds[~ds["file name"].isin(filename_to_remove)]
+
+# Controlliamo quali righe abbiamo rimosso (Solo righe dove è presente la regina)
+print(ds["queen presence"].value_counts())
+print(ds2["queen presence"].value_counts())
+
+# Le righe del dataset in cui la regina è assente, per dare un razionale dietro la scelta della rimozione di
+# segment1 e segment0 dove singoli. Riscrivere codice TODO
+# queen_absent_set = set(ds.loc[ds["queen presence"] == 0]["file name"])
+
+
