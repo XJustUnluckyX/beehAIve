7c151b5e53125a4cbc7929d6579f7288e98a0a01
MonTheDog <cate.party@gmail.com>
2024-01-05 04:45:22 +0100

    Finished CNN evaluation Log and added various code snippets

--- src/ai/CNN.py
+++ src/ai/CNN.py
@@ -1,10 +1,12 @@
 import keras.metrics
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 import pandas as pd
 import numpy as np
 from PIL import Image
 from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 # Prendiamo i dati da dare al modello
 ds = pd.read_csv("dataset/TEST1_CNN_Dataset.csv")
@@ -29,9 +31,9 @@ X_train = np.asarray(X_train)
 # epochs = [3, 4, 5, 6, 7, 8, 9, 10]
 # batch_size = [8, 16, 32]
 pooling = "MaxPooling" # {MaxPooling, AveragePooling}
-dense_neurons = 128 # {32, 64, 128, 256}
+dense_neurons = 256 # {32, 64, 128, 256}
 optimizer = "adam" # {SGD, rmsprop, adam, adamw, adadelta, adagrad, adamax, adafactor, nadam, ftrl}
-epochs = 3 # { 3, 4, 5, 6, 7, 8, 9, 10 }
+epochs = 4 # { 3, 4, 5, 6, 7, 8, 9, 10 }
 batch_size = 16 # {8, 16, 32}
 
 val_loss_per_fold = []
@@ -52,16 +54,18 @@ fold_no = 1
 for train, test in skf.split(X_train, y_train):
     # Creazione del modello
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation="relu", input_shape=(480, 640, 3)))
+    model.add(Conv2D(32, (5, 5), activation="relu", input_shape=(480, 640, 3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(64, (3, 3), activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (3, 3), activation="relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(dense_neurons, activation="relu"))
     model.add(Dense(1, activation="sigmoid"))
 
     # Compilazione del modello
-    model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=[keras.metrics.BinaryAccuracy(), keras.metrics.Precision(name="precision"), keras.metrics.AUC(name="auc"),])
+    model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy", keras.metrics.Precision(name="precision"), keras.metrics.AUC(name="auc")])
 
     print(f"--------- Fold n. {fold_no} ---------")
     # Addestramento del modello
@@ -70,7 +74,7 @@ for train, test in skf.split(X_train, y_train):
     # Valutazione del modello
     eval = model.evaluate(X_train[test], y_train[test], verbose = 0)
 
-    # Utilizziamo l'oggetto eval per ottenere le metriche di testing
+    # Utilizziamo l'oggetto eval per salvare le metriche di testing
     val_loss_per_fold.append(eval[0])
     val_accuracy_per_fold.append(eval[1] * 100)
     val_precision_per_fold.append(eval[2] * 100)
@@ -78,9 +82,30 @@ for train, test in skf.split(X_train, y_train):
 
     # Utilizziamo la history per salvare le metriche di training (per diagnosticare eventuale overfitting)
     training_loss_per_fold.append(history.history["loss"][epochs-1])
-    training_accuracy_per_fold.append(history.history["binary_accuracy"][epochs-1] * 100)
+    training_accuracy_per_fold.append(history.history["accuracy"][epochs-1] * 100)
     training_precision_per_fold.append(history.history["precision"][epochs-1] * 100)
     training_AUC_per_fold.append(history.history["auc"][epochs-1])
+
+    # Prediction è un array di probabilità eva trasformato in una previsione
+    # Questo codice rappresenta un test, la predict effettiva va fatta sul DEMO Set
+    prediction = model.predict(X_train[test])
+    prediction = prediction.flatten()
+    prediction = np.where(prediction > 0.5, 1, 0)
+    print(prediction)
+    print(y_train[test])
+
+    # Produce la Confusion Matrix, i quadranti interessanti sono in alto a sx (TN) e in alto a dx (FP)
+    # Questo perchè ci interessa vedere quante volte prende correttamente l'assenza della regina
+    # e quante volte la manca. In particolare TN va massimizzato e FP va minimizzato
+    # Accettiamo qualche FN perchè meglio avvertire l'apicoltore di un pericolo inesistente
+    # che non avvertirlo di un pericolo effettivo, quindi teniamo d'occhio il quadrante in basso a dx
+    # ma non ci soffermiamo troppo sul suo valore (Andrebbe minimizzato)
+    # result = confusion_matrix(y_train[test], prediction)
+    # disp = ConfusionMatrixDisplay(confusion_matrix=result)
+    # disp.plot()
+    # plt.show()
+
+    # Dopo aver fatto la k fold bisogna fare di nuovo training su tutto il dataset e settare sulla Demo
 
     # Iterazione k-fold validation
     fold_no+=1
@@ -93,13 +118,14 @@ try:
                 f"Pooling: {pooling}, Dense_neurons: {dense_neurons}, Optimizer: {optimizer}, "
                 f"Epochs: {epochs}, Batch_Size: {batch_size}\n\n"
                 "VALUTAZIONE TRAINING:\n"
-                f"Training_Loss: {np.mean(training_loss_per_fold):.4f}, Training_Accuracy: {np.mean(training_accuracy_per_fold):.4f}%, "
-                f"Training_Precision: {np.mean(training_precision_per_fold):.4f}%, Training_AUC: {np.mean(training_AUC_per_fold):.4f}\n\n"
+                f"Training Loss: {np.mean(training_loss_per_fold):.4f}, Training Accuracy: {np.mean(training_accuracy_per_fold):.4f}%, "
+                f"Training Precision: {np.mean(training_precision_per_fold):.4f}%, Training AUC: {np.mean(training_AUC_per_fold):.4f}\n\n"
                 "VALUTAZIONE TESTING:\n"
                 f"Test Loss: {np.mean(val_loss_per_fold):.4f}, Test Accuracy: {np.mean(val_accuracy_per_fold):.4f}%, "
                 f"Test Precision: {np.mean(val_precision_per_fold):.4f}%, Test AUC: {np.mean(val_AUC_per_fold):.4f}\n")
 except FileNotFoundError:
     print("File not found.")
+
 
 
 # SPERIMENTAZIONE EMPIRICA:
