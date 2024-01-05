4d35e79cbce603809bba88e6e1abe1d78a63f51e
gnicolo00 <ngallotta00@gmail.com>
2024-01-05 16:23:03 +0100

    Adjusted code and changed the file structure

---
+++ src/ai/development/ML/2. data_preparation.py
@@ -0,0 +1,8 @@
+
+
+
+# Creazione di due nuove colonne "temp diff" e "humidity diff", che rappresentano rispettivamente
+# la differenza tra la temperatura esterna e interna all'arnia, e
+# la differenza tra l'umidità interna ed esterna all'arnia
+dataset["temp diff"] = dataset["hive temp"] - dataset["weather temp"]
+dataset["humidity diff"] = dataset["hive humidity"] - dataset["weather humidity"]
