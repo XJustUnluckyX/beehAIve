572edf91d945065ca8079cd03fa3a7c2ac4a0daa
gnicolo00 <ngallotta00@gmail.com>
2024-01-17 23:09:16 +0100

    Adjustments for DashboardService class

--- src/main/java/it/unisa/c10/beehAIve/service/gestioneArnie/DashboardService.java
+++ src/main/java/it/unisa/c10/beehAIve/service/gestioneArnie/DashboardService.java
@@ -4,11 +4,13 @@ import it.unisa.c10.beehAIve.persistence.dao.AnomalyDAO;
 import it.unisa.c10.beehAIve.persistence.dao.HiveDAO;
 import it.unisa.c10.beehAIve.persistence.dao.OperationDAO;
 import it.unisa.c10.beehAIve.persistence.entities.Anomaly;
+import it.unisa.c10.beehAIve.persistence.entities.Beekeeper;
 import it.unisa.c10.beehAIve.persistence.entities.Hive;
 import it.unisa.c10.beehAIve.persistence.entities.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -25,41 +27,72 @@ La seguente classe deve supportare le seguenti operazioni:
 
 @Service
 public class DashboardService {
-
-  private HiveDAO hiveDAO;
-  private OperationDAO operationDAO;
-  private AnomalyDAO anomalyDAO;
+  private final HiveDAO hiveDAO;
+  private final OperationDAO operationDAO;
+  private final AnomalyDAO anomalyDAO;
 
   @Autowired
-  public DashboardService(HiveDAO hiveDao, OperationDAO operationDAO,AnomalyDAO anomalyDAO){
-    this.hiveDAO = hiveDao;
+  public DashboardService(HiveDAO hiveDAO, OperationDAO operationDAO,AnomalyDAO anomalyDAO){
+    this.hiveDAO = hiveDAO;
     this.operationDAO = operationDAO;
     this.anomalyDAO = anomalyDAO;
   }
 
-  public void creationhive(Hive hive){
+  // Metodo per ricercare l'arnia nel database, così da evitare ripetizioni nel codice
+  public Hive getHive(int hiveId) {
+    // Ricerca dell'arnia nel database
+    Optional<Hive> optionalHive = hiveDAO.findById(hiveId);
+
+    // Controllo sull'esistenza dell'arnia nel database
+    if (optionalHive.isPresent()) {
+      return optionalHive.get();
+    } else {
+      throw new NullPointerException("Hive not found for ID: " + hiveId);
+    }
+  }
+
+  public void createHive(int hiveId, String nickname, String hiveType, String beekeeperEmail,
+                         String beeSpecies){
+    Hive hive = new Hive();
+
+    hive.setId(hiveId);
+    hive.setNickname(nickname);
+    hive.setHiveType(hiveType);
+    hive.setCreationDate(LocalDate.now());
+    hive.setBeekeeperEmail(beekeeperEmail);
+    hive.setBeeSpecies(beeSpecies);
+
     hiveDAO.save(hive);
   }
 
-  public void ModifyHive(int id, String Nickname, String HiveType, String Bees_Type){
-    Optional<Hive> tempHive = hiveDAO.findById(id);
+  public void modifyHive(int hiveId, String nickname, String hiveType, String beeSpecies){
+    Hive hive = getHive(hiveId);
 
-    tempHive.get().setNickname(Nickname);
-    tempHive.get().setHiveType(HiveType);
-    tempHive.get().setBeeSpecies(Bees_Type);
+    hive.setNickname(nickname);
+    hive.setHiveType(hiveType);
+    hive.setBeeSpecies(beeSpecies);
 
-    hiveDAO.save(tempHive.get());
+    hiveDAO.save(hive);
   }
 
-  public void DeleteHive(int id){
+  public void deleteHive(int id){
     hiveDAO.deleteById(id);
   }
 
-  public List<Hive> ShowAllHive(){
-    return hiveDAO.findAll();
+  public List<Hive> getAllHivesByBeekeeper(String beekeeperEmail){
+    return hiveDAO.findByBeekeeperEmail(beekeeperEmail);
   }
 
-  public List ShowAllHiveByAnomaly(String anomaly){
+  public List<Hive> getAllHivesByBeekeeperAndNickname(String beekeeperEmail, String nickname) {
+    return hiveDAO.findByNicknameContainingAndBeekeeperEmail(nickname, beekeeperEmail);
+  }
+
+  public List<Hive> getAllHivesByBeekeeperAndDateRange(String beekeeperEmail,
+                                                       LocalDate date1, LocalDate date2) {
+    return hiveDAO.findByCreationDateBetween(date1,date2);
+  }
+
+  public List<Hive> getHivesByAnomalies(String anomaly){
     int i;
     Optional<Hive> hive;
     List<Hive> hives = new ArrayList<>();
@@ -93,11 +126,4 @@ public class DashboardService {
     return hives;
 
   }
-
-  public Hive ShowHive(int HiveId){
-
-    return hiveDAO.findById(HiveId).get();
-
-  }
-
 }
