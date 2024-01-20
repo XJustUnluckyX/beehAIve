c2648e2c02f88e8d8c20cc07f5e2ed16b1a2b7f0
gnicolo00 <ngallotta00@gmail.com>
2024-01-20 23:01:34 +0100

    Moved regex methods from ProfileService to AccessController and ProfileController

--- src/main/java/it/unisa/c10/beehAIve/service/gestioneUtente/ProfileService.java
+++ src/main/java/it/unisa/c10/beehAIve/service/gestioneUtente/ProfileService.java
@@ -7,16 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-
-/*
-La seguente classe deve supportare le seguenti operazioni:
-1. Registrare l'apicoltore.
-2. Loggare dentro l'apicoltore.
-3. Loggare fuori l'apicoltore.
-4. Modificare i dati personali dell'apicoltore.
-5. Modificare la password dell'apicoltore.
- */
-
 @Service
 public class ProfileService {
   private final BeekeeperDAO beekeerperDAO;
@@ -24,37 +14,6 @@ public class ProfileService {
   @Autowired
   public ProfileService(BeekeeperDAO beekeerperdao) {
     this.beekeerperDAO = beekeerperdao;
-  }
-
-  public boolean regexEmail (String email) {
-    String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{5,}$";
-    return email.matches(emailRegex);
-  }
-
-  public boolean regexPassword (String password) {
-    String passwordRegex =
-        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@.$!%*?&])[A-Za-z\\d@.$!%*?&]{8,}$";
-    return password.matches(passwordRegex);
-  }
-
-  public boolean regexFirstName (String firstName) {
-    String firstNameRegex = "^[A-Z][a-z'-]+(?: [A-Z][a-z'-]+)*$";
-    return firstName.matches(firstNameRegex);
-  }
-
-  public boolean regexLastName (String lastName) {
-    String lastNameRegex = "^[A-Z][a-z'-]+(?: [A-Z][a-z'-]+)*$";
-    return lastName.matches(lastNameRegex);
-  }
-
-  public boolean regexCompanyName (String companyName) {
-    String companyRegex = "^[a-zA-Z0-9\\s.'\",&()-]+$";
-    return companyName.matches(companyRegex);
-  }
-
-  public boolean regexCompanyPiva (String companyPiva) {
-    String companyPivaRegex = "^[\\d-]{9,}$";
-    return companyPiva.matches(companyPivaRegex);
   }
 
   public Beekeeper registration(String email, String password, String firstName, String lastName,
@@ -67,20 +26,6 @@ public class ProfileService {
     beekeeper.setCompanyName(companyName);
     beekeeper.setCompanyPiva(companyPiva);
     return beekeerperDAO.save(beekeeper);
-  }
-
-  public boolean emailExists(String email) {
-    return (beekeerperDAO.findById(email)).isPresent();
-  }
-
-  public boolean pivaExists(String piva) {
-    return !(beekeerperDAO.findByCompanyPiva(piva).isEmpty());
-  }
-
-  public boolean userExists(String email, String password) {
-    Beekeeper beekeeper = new Beekeeper();
-    beekeeper.setPasswordhash(password);
-    return !(beekeerperDAO.findByEmailAndPasswordhash(email,beekeeper.getPasswordhash()).isEmpty());
   }
 
   public Beekeeper changeInfo(String email, String firstName, String lastName, String companyName) {
@@ -107,9 +52,22 @@ public class ProfileService {
     }
   }
 
+  public boolean emailExists(String email) {
+    return (beekeerperDAO.findById(email)).isPresent();
+  }
+
+  public boolean pivaExists(String piva) {
+    return !(beekeerperDAO.findByCompanyPiva(piva).isEmpty());
+  }
+
+  public boolean userExists(String email, String password) {
+    Beekeeper beekeeper = new Beekeeper();
+    beekeeper.setPasswordhash(password);
+    return !(beekeerperDAO.findByEmailAndPasswordhash(email,beekeeper.getPasswordhash()).isEmpty());
+  }
+
   public Optional<Beekeeper> findBeekeeper(String email) {
     return beekeerperDAO.findById(email);
   }
-
 }
 
