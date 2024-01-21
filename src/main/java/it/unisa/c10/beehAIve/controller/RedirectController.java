a602da0f29580226aa8cdb8f8c8c75746eb89b03
MonTheDog <cate.party@gmail.com>
2024-01-21 17:05:14 +0100

    Fixed RedirectController and bug in Operations Hive

--- src/main/java/it/unisa/c10/beehAIve/controller/RedirectController.java
+++ src/main/java/it/unisa/c10/beehAIve/controller/RedirectController.java
@@ -12,20 +12,8 @@ public class RedirectController {
   public String home (Model model) {
     return "index";
   }
-//  @GetMapping("/login-page")
-//  public String login (Model model) {
-//    return "login-page";
-//  }
-//  @GetMapping("/registration-page")
-//  public String register (Model model) {
-//    return "registration-page";
-//  }
   @GetMapping("/subscription-page")
   public String subscription (Model model) { return "subscription-page";}
-  @GetMapping("/creation-hive")
-  public String creation (Model model) { return "hive/creation-hive";}
-  @GetMapping("/parameters-hive")
-  public String parameter (Model model) { return "hive/parameters-hive";}
   @GetMapping("/contact-us")
   public String contact (Model model) { return "/contact-us";}
   @GetMapping("/about-us")
@@ -33,20 +21,6 @@ public class RedirectController {
   @GetMapping("/sensor-spec")
   public String sensor (Model model) { return "/sensor-spec";}
 
-  @GetMapping("/error404")
-  public String er404 (Model model) { return "error/404";}
-  @GetMapping("/error500")
-  public String er500 (Model model) { return "error/500";}
-  @GetMapping("/subscription-test")
-  public String subscriptionTest(Model model) {return "subscription-test";}
 
-  @GetMapping("/user-page")
-  public String user (Model model) {
-    return "/user-page";
-  }
 
-  @GetMapping("/calendar-planning")
-  public String calendarTest (Model model) {
-    return "calendar-page";
-  }
 }
